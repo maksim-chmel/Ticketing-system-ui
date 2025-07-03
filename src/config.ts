9c01040d208a57b3b0a6afebfd18ef07ec5e952a
vodkopei <vodkopei.uk@gmail.com>
2025-07-03 14:13:58 +0300

    Fix

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 // src/config.ts
-const BASE_URL = "http://100.70.1.11:5101/api"; // Пропиши нужный порт и адрес
+const BASE_URL = "http://it-help.almi.odesa.ua:5101/api";
 
 export default BASE_URL;
