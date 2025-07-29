ae403ff784b8dc48f4ff607dee285fe28cb58899
vodkopei <vodkopei.uk@gmail.com>
2025-07-29 10:47:44 +0300

    config.ts Fix

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 // src/config.ts
-const BASE_URL = "https://helpalm.varit.xyz:5101/api";
+//const BASE_URL = "https://helpalm.varit.xyz:5101/api";
+const BASE_URL = "https://helpalm.varit.xyz/api";
 
 export default BASE_URL;
